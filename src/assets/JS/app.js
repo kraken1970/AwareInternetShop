c38931ed0df78efa33d5c21d0a3340b4cae73e96
kraken1970 <kraken@hi.ru>
2018-03-02 00:47:35 +0300

    l 5 4:25

--- src/assets/JS/app.js
+++ src/assets/JS/app.js
@@ -16,20 +16,20 @@ $(function(){
 		return false;
 	});
 
-	$('.styler').styler();
+	// $('.styler').styler();
 
-	$( "#slider-range" ).slider({
-		range: true,
-		min: 0,
-		max: 500,
-		values: [ 75, 300 ],
-		slide: function( event, ui ) {
-			$( "#amount" ).val( "$" + ui.values[ 0 ] );
-			$( "#amount1" ).val( "$" + ui.values[ 1 ] );
-		}
-    });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) );
-    $( "#amount1" ).val( "$" + $( "#slider-range" ).slider( "values", 1 ) );
+	// $( "#slider-range" ).slider({
+	// 	range: true,
+	// 	min: 0,
+	// 	max: 500,
+	// 	values: [ 75, 300 ],
+	// 	slide: function( event, ui ) {
+	// 		$( "#amount" ).val( "$" + ui.values[ 0 ] );
+	// 		$( "#amount1" ).val( "$" + ui.values[ 1 ] );
+	// 	}
+  //   });
+  //   $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) );
+  //   $( "#amount1" ).val( "$" + $( "#slider-range" ).slider( "values", 1 ) );
 	
 
 });
